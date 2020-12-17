5c629d5bdac4f2f31c0f1a24fe7fbb51ffd71306
ninab15 <nina1501@live.dk>
2020-12-17 16:22:37 +0100

    Alt virker WUUHUU

--- script.js
+++ script.js
@@ -1,3 +1,94 @@
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+
+
+
+    gsap.registerPlugin(ScrollTrigger);
+
+
+    gsap.from(".stagger", {
+        opacity: 0,
+        y: 100,
+        duration: 1.2,
+        stagger: 0.5,
+        scrollTrigger: {
+            trigger: ".stagger",
+            start: "top bottom",
+            end: "top top",
+            markers: false,
+        }
+
+    });
+
+
+    gsap.from(".stagger2", {
+        opacity: 0,
+        y: 100,
+        duration: 1.2,
+        stagger: 0.5,
+        scrollTrigger: {
+            trigger: ".stagger2",
+            start: "top bottom",
+            end: "top top",
+            markers: false,
+        }
+
+    });
+
+    gsap.from(".stagger3", {
+        opacity: 0,
+        y: 100,
+        duration: 1.2,
+        stagger: 0.5,
+        scrollTrigger: {
+            trigger: ".stagger3",
+            start: "top bottom",
+            end: "top top",
+            markers: false,
+        }
+
+    });
+
+    gsap.from(".hero-h1", {
+        opacity: 0,
+        y: 100,
+        duration: 1.2,
+        stagger: 0.5,
+
+    });
+
+    let config = {
+        strength: 1
+    };
+
+    gsap.set(".ikoner", {
+        xPercent: -10,
+        x: -1
+    });
+
+    gsap.to(".ikoner", {
+        repeat: -1,
+        yoyo: true,
+        x: 1,
+        duration: 0.2,
+        ease: "power1.inOut",
+        modifiers: {
+            x: gsap.utils.unitize(value => value * config.strength, "px")
+        }
+    });
+
+    gsap.to(config, {
+        strength: 5,
+        ease: "none",
+        scrollTrigger: {
+            // defaults to using the window as the trigger, starting at the top, ending at the bottom.
+            scrub: true
+        }
+    });
+
+}
+
 const cookieContainer = document.querySelector(".cookie-container");
 const cookieButton = document.querySelector(".cookie-btn");
 
@@ -11,86 +102,3 @@ setTimeout(() => {
         cookieContainer.classList.add("active");
     }
 }, 2000);
-
-gsap.registerPlugin(ScrollTrigger);
-
-
-gsap.from(".stagger", {
-    opacity: 0,
-    y: 100,
-    duration: 1.2,
-    stagger: 0.5,
-    scrollTrigger: {
-        trigger: ".stagger",
-        start: "top bottom",
-        end: "top top",
-        markers: false,
-    }
-
-});
-
-
-gsap.from(".stagger2", {
-    opacity: 0,
-    y: 100,
-    duration: 1.2,
-    stagger: 0.5,
-    scrollTrigger: {
-        trigger: ".stagger2",
-        start: "top bottom",
-        end: "top top",
-        markers: false,
-    }
-
-});
-
-gsap.from(".stagger3", {
-    opacity: 0,
-    y: 100,
-    duration: 1.2,
-    stagger: 0.5,
-    scrollTrigger: {
-        trigger: ".stagger3",
-        start: "top bottom",
-        end: "top top",
-        markers: false,
-    }
-
-});
-
-gsap.from(".hero-h1", {
-    opacity: 0,
-    y: 100,
-    duration: 1.2,
-    stagger: 0.5,
-
-});
-
-let config = {
-    strength: 1
-};
-
-gsap.set(".ikoner", {
-    xPercent: -10,
-    x: -1
-});
-
-gsap.to(".ikoner", {
-    repeat: -1,
-    yoyo: true,
-    x: 1,
-    duration: 0.2,
-    ease: "power1.inOut",
-    modifiers: {
-        x: gsap.utils.unitize(value => value * config.strength, "px")
-    }
-});
-
-gsap.to(config, {
-    strength: 5,
-    ease: "none",
-    scrollTrigger: {
-        // defaults to using the window as the trigger, starting at the top, ending at the bottom.
-        scrub: true
-    }
-});
